bd87c98a8c9ed81d4f6ee318ba688ea1a5e7b0dc
maifeeulasad <maifeeulasad@gmail.com>
2020-12-04 18:23:14 +0600

    basic ui

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,53 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Gallery from "react-mua-gallery";
+// @ts-ignore
+import "react-mua-gallery/dist/dist/index.css";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+	
+        <Gallery
+          galleryName={"Gallery"}
+          owner={"MUA"}
+          slogan={
+            "Yet another fun template by MUA, based on other open-sourced projects for another open-sourced projects"
+          }
+          licenseLink={"https://www.github.com/maifeeulasad"}
+          socials={[
+            { socialNetwork: "github", socialHandle: "maifee" },
+            { socialNetwork: "facebook", socialHandle: "maifee" },
+            { socialNetwork: "twitter", socialHandle: "maifee" }
+          ]}
+          images={[
+            {
+              url:
+                "https://raw.githubusercontent.com/maifeeulasad/Icon-collection/master/background/horizontal/blurry/01.jpg",
+              title: "test0",
+              details: "test0_"
+            },
+            {
+              url:
+                "https://raw.githubusercontent.com/maifeeulasad/Icon-collection/master/background/horizontal/blurry/02.jpg",
+              title: "test1",
+              details: "test1_"
+            },
+            {
+              url:
+                "https://raw.githubusercontent.com/maifeeulasad/Icon-collection/master/background/horizontal/blurry/03.jpg",
+              title: "test2",
+              details: "test2_"
+            },
+            {
+              url:
+                "https://raw.githubusercontent.com/maifeeulasad/Icon-collection/master/background/horizontal/blurry/04.jpg",
+              title: "test3",
+              details: "test3_"
+            }
+          ]}
+          licenseTitle={"lOve"}
+        />
   </React.StrictMode>,
   document.getElementById('root')
 );
