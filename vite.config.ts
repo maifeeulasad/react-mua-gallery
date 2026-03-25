1630f3dbd1a7d5f72528e41b4f54646a6040905b
Maifee Ul Asad <maifeeulasad@gmail.com>
2026-03-25 21:32:18 +0600

    [config]: vite config for static and lib;

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,35 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    lib: {
-      entry: 'src/index.tsx',
-      name: 'ReactMUAGallery',
-      fileName: (format) => `index.${format}.js`,
-    },
-    rollupOptions: {
-      external: ['react', 'react-dom'],
-      output: {
-        exports: 'named',
-        globals: {
-          react: 'React',
-          'react-dom': 'ReactDOM',
+export default defineConfig(({ mode }) => {
+  if (mode === 'lib') {
+    return {
+      plugins: [react()],
+      build: {
+        outDir: 'dist',
+        lib: {
+          entry: 'src/index.tsx',
+          fileName: (format) => `index.${format}.js`,
+        },
+        rollupOptions: {
+          external: ['react', 'react-dom'],
+          output: {
+            exports: 'named',
+            globals: {
+              react: 'React',
+              'react-dom': 'ReactDOM',
+            },
+          },
         },
       },
+    };
+  }
+
+  return {
+    plugins: [react()],
+    build: {
+      outDir: 'example-dist',
+      emptyOutDir: true,
     },
-  },
+  };
 });
