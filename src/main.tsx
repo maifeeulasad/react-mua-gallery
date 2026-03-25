8e332f54991bbe0812937d95ed215dc02eaf54cc
Maifee Ul Asad <maifeeulasad@gmail.com>
2026-03-25 21:13:21 +0600

    [export]: modular import for example gallery;

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import ExampleGallery from './ExampleGallery'
+import { ExampleGallery } from './ExampleGallery'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
