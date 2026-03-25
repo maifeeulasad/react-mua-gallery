05c28adc9161c19e63415c1c70c320c30ef8f867
Maifee Ul Asad <maifeeulasad@gmail.com>
2026-03-25 15:45:31 +0600

    [config]: named export for vite build;

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,7 @@ export default defineConfig({
     rollupOptions: {
       external: ['react', 'react-dom'],
       output: {
+        exports: 'named',
         globals: {
           react: 'React',
           'react-dom': 'ReactDOM',
