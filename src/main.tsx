0c5df4249ab68e0f419751b6a73f32eb988f78a9
Maifee Ul Asad <maifeeulasad@gmail.com>
2026-03-25 21:09:39 +0600

    [ui]: basic react dom w example gallery;

---
+++ src/main.tsx
@@ -0,0 +1,9 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import ExampleGallery from './ExampleGallery'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <ExampleGallery />
+  </StrictMode>,
+)
